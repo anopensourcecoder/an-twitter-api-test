afa1d7a70e22c19e28f391515874cd1ef7f7bb25
an <anopensourcecoder@gmail.com>
2019-06-19 15:37:04 +1000

    Updates

---
+++ precache-manifest.21368d7530e2840f8a759cd51b0c02d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6061f211569f424cf90a3686951d64a",
+    "url": "/an-twitter-api-test/index.html"
+  },
+  {
+    "revision": "461d4adeaafe7b5f1fc3",
+    "url": "/an-twitter-api-test/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/an-twitter-api-test/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "461d4adeaafe7b5f1fc3",
+    "url": "/an-twitter-api-test/static/js/main.3bebebec.chunk.js"
+  },
+  {
+    "revision": "8c7ea69242c1f27383a9",
+    "url": "/an-twitter-api-test/static/js/runtime~main.23c21e1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/an-twitter-api-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
